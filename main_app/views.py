021eb049c535a321d2640963baa2ab8e2a7e6a24
Duwaysan <kduwaysan@gmail.com>
2025-10-28 16:48:08 +0300

    Initial commit - backend

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# Create your views here.
+
+# Define the home view
+class Home(APIView):
+  def get(self, request):
+    content = {'message': 'Welcome to the IT-Ticket-System api home route!'}
+    return Response(content)
